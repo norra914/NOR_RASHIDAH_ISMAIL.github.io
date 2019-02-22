e7cef1f983b564024de89bb3d5e1c12a2a58984f
norra914 <47876946+norra914@users.noreply.github.com>
2019-02-22 12:47:03 +0800

    Add files via upload

---
+++ _site yml.R
@@ -0,0 +1,18 @@
+name: "Nor Rashidah Bt Paujah @ Ismail"
+output_dir: "."
+navbar:
+  title:"Nor Rashidah Bt Paujah @ Ismail"
+left:
+  -text:"writing"
+href: index.html
+right:
+  -icon:fa-envelope fa-lg
+href:contact.html
+
+icon:fa-github fa-1g
+href:https://github.com/norra914/NOR_RASHIDAH_ISMAIL.github.io
+
+output:
+  html_document:
+  theme:paper
+css:'styles.css'
